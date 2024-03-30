6880a025c59ad437892a318c415ddcc2aad3c401
Moiii0402 <huutrong0424@gmail.com>
2024-03-30 19:18:40 +0700

    Add new project food

---
+++ php.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foodz</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/responsive.css">
+    <!-- Include other CSS and JavaScript files -->
+</head>
+<body>
+    <!-- header -->
+    <?php include 'header.php'; ?>
+    <!-- header end -->
+
+    <!-- hero -->
+    <?php include 'hero.php'; ?>
+    <!-- hero end -->
+
+    <!-- service -->
+    <?php include 'service.php'; ?>
+    <!-- service end -->
+
+    <!-- about -->
+    <?php include 'about.php'; ?>
+    <!-- about end -->
+
+    <!-- special -->
+    <?php include 'special.php'; ?>
+    <!-- special end-->
+
+    <!-- form -->
+    <?php include 'form.php'; ?>
+    <!-- form end -->
+
+    <!-- blog -->
+    <?php include 'blog.php'; ?>
+    <!-- blog end -->
+
+    <!-- footer -->
+    <?php include 'footer.php'; ?>
+    <!-- footer end -->
+
+    <!-- Include JavaScript files -->
+<header class="header">
+    <div class="container">
+        <!-- Your header content goes here -->
+    </div>
+</header>
+</body>
+</html>
